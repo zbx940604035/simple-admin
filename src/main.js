9c89ee61b4903b332f1a69f0511669a907d9766f
duans <940604035@qqcom>
2021-08-23 00:31:40 +0800

    0823 搭建登录文件，配置eslint，配置路由

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
+import router from './router'
+import '@/assets/scss'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 new Vue({
   store,
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,94 +1,68 @@
+const path = require('path')
 module.exports = {
-
-      // 部署应用时的基本 URL
-    // publicPath: process.env.NODE_ENV === 'production' ? '127.0.0.1:9001' : '127.0.0.1:8080',
-
-    // build时构建文件的目录 构建时传入 --no-clean 可关闭该行为
-    outputDir: 'dist',
-
-    // build时放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
-    assetsDir: '',
-
-    // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
-    indexPath: 'index.html',
-
-    // 默认在生成的静态资源文件名中包含hash以控制缓存
-    filenameHashing: true,
-
-    // 构建多页面应用，页面的配置
-    pages: {
-        index: {
-            // page 的入口
-            entry: 'src/main.js',
-            // 模板来源
-            // 在 dist/index.html 的输出
-            filename: 'index.html',
-            // 当使用 title 选项时，
-            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-            title: 'Index Page',
-            // 在这个页面中包含的块，默认情况下会包含
-            // 提取出来的通用 chunk 和 vendor chunk。
-            chunks: ['chunk-vendors', 'chunk-common', 'index']
+    publicPath: './', // 基本路径
+    outputDir: 'dist', // 输出文件目录
+    lintOnSave: false, // eslint-loader 是否在保存的时候检查
+    // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+    // webpack配置
+    chainWebpack: (config) => {
+    },
+    configureWebpack: (config) => {
+        if (process.env.NODE_ENV === 'production') {
+            // 为生产环境修改配置...
+            config.mode = 'production'
+        } else {
+            // 为开发环境修改配置...
+            config.mode = 'development'
         }
-        // 当使用只有入口的字符串格式时，
-        // 模板会被推导为 `public/subpage.html`
-        // 并且如果找不到的话，就回退到 `public/index.html`。
-        // 输出文件名会被推导为 `subpage.html`。
-        // subpage: 'src/subpage/main.js'
+        Object.assign(config, {
+            // 开发生产共同配置
+            resolve: {
+                extensions: ['.js', '.vue', '.json', '.scss'],
+                alias: {
+                    '@': path.resolve(__dirname, './src'),
+                    '@c': path.resolve(__dirname, './src/components'),
+                    '@w': path.resolve(__dirname, './src/views')
+                } // 别名配置
+            }
+        })
     },
-
-    // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码 (在生产构建时禁用 eslint-loader)
-    // lintOnSave: process.env.NODE_ENV !== 'production',
-
-    // 是否使用包含运行时编译器的 Vue 构建版本
-    runtimeCompiler: false,
-
-    // Babel 显式转译列表
-    transpileDependencies: [],
-
-    // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建
-    productionSourceMap: true,
-
-    // 设置生成的 HTML 中 <link rel="stylesheet"> 和 <script> 标签的 crossorigin 属性（注：仅影响构建时注入的标签）
-    crossorigin: '',
-
-    // 在生成的 HTML 中的 <link rel="stylesheet"> 和 <script> 标签上启用 Subresource Integrity (SRI)
-    integrity: false,
-
-    // 如果这个值是一个对象，则会通过 webpack-merge 合并到最终的配置中
-    // 如果你需要基于环境有条件地配置行为，或者想要直接修改配置，那就换成一个函数 (该函数会在环境变量被设置之后懒执行)。该方法的第一个参数会收到已经解析好的配置。在函数内，你可以直接修改配置，或者返回一个将会被合并的对象
-    configureWebpack: {},
-
-    // 对内部的 webpack 配置（比如修改、增加Loader选项）(链式操作)
-    chainWebpack: () =>{
-
-    },
-
-    // css的处理
+    productionSourceMap: false, // 生产环境是否生成 sourceMap 文件
+    // css相关配置
     css: {
-        // 当为true时，css文件名可省略 module 默认为 false
-        requireModuleExtension: true,
-        // 是否将组件中的 CSS 提取至一个独立的 CSS 文件中,当作为一个库构建时，你也可以将其设置为 false 免得用户自己导入 CSS
-        // 默认生产环境下是 true，开发环境下是 false
-        extract: false,
-        // 是否为 CSS 开启 source map。设置为 true 之后可能会影响构建的性能
-        sourceMap: false,
-        //向 CSS 相关的 loader 传递选项(支持 css-loader postcss-loader sass-loader less-loader stylus-loader)
+        extract: false, // 是否使用css分离插件 ExtractTextPlugin
+        sourceMap: false, // 开启 CSS source maps?
         loaderOptions: {
-            css: {},
+            css: {}, // 这里的选项会传递给 css-loader
             sass: {
-                prependData: `@import "./src/assets/base";`,
+                prependData: `@import "./src/assets/scss";`,
             },
-        }
+        }, // css预设器配置项 详见https://cli.vuejs.org/zh/config/#css-loaderoptions
+        modules: false // 启用 CSS modules for all css / pre-processor files.
     },
-    // 所有 webpack-dev-server 的选项都支持
-    devServer: {},
-
-    // 是否为 Babel 或 TypeScript 使用 thread-loader
-    parallel: require('os').cpus().length > 1,
-
-    // 向 PWA 插件传递选项
-    pwa: {},
-
-    // 可以用来传递任何第三方插件选项
+    parallel: require('os').cpus().length > 1, // 是否为 Babel 或 TypeScript 使用 thread-loader。该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建。
+    pwa: {}, // PWA 插件相关配置 see https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
+    // webpack-dev-server 相关配置
+    devServer: {
+        open: process.platform === 'darwin',
+        host: '0.0.0.0', // 允许外部ip访问
+        port: 9004, // 端口
+        https: false, // 启用https
+        overlay: {
+            warnings: true,
+            errors: true
+        }, // 错误、警告在页面弹出
+        proxy: {
+            '/api': {
+                target: 'http://www.baidu.com/api',
+                changeOrigin: true, // 允许websockets跨域
+                // ws: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        } // 代理转发配置，用于调试环境
+    },
+    // 第三方插件配置
+    pluginOptions: {}
 }
